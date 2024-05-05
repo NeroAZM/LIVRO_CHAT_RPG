422893d2c79b095a4d6c35e984f06f630c021fb0
NeroAZM <161223249+NeroAZM@users.noreply.github.com>
2024-05-05 10:46:54 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void limpar() {
+    system("cls");
+}
+
+int main()
+
+{
+
+    //Declaracao de variaveis
+    int escolha1, escolha2;
+    
+    //CABECALHO
+    cout<<"Livro CHATRPG - Criado e apresentado por Lucas de Amorim Coelho e Thiago Gualberto";
+    cout<<"\nObservacao: esse livro possui tres finais, dito isso, bom jogo e divirta-se!";
+    limpar();//limpar console
+    
+    //Dialogo 1
+        cout<<"\n\nDurante o periodo da idade medieval, em uma cidade chamada Haprinas, um homem chamado Rodrigo, fascinado pela natureza das florestas e animais, almejava se tornar mais proximo da mesma, era tanto a admiracao que sentia que tinha vontade de se tornar ate mesmo um simbionte com ela.";
+        usleep(2000000);
+
+        limpar();//limpar console
+
+        //Dialogo 2
+        cout<<"\nPorem as pessoas com quem convivia e o restante dos cidadaos nao tinham nenhuma preocupacao com a flora e fauna local, com a ideia de se tornar um com a natureza, para defende-la, ele se tornou obstinado, tentando por varios anos diversas formas diferentes, no entanto, ele sempre acabava falhando.";
+        usleep(5000000);
+
+        limpar();//limpar console
+
+        //Dialogo 3
+        cout<<"\nE, com isso, ele no seu maior estado de desespero e loucura, buscou o conhecimento necessario para realizar seu objetivo atraves de livros proibidos profanos e obscuros, onde ele descobriu um metodo que para cumprir os requisitos, seria necessario obter o sangue de inumeros seres humanos para conseguir fazer com que aos poucos, o ritual transforme-o lentamente um-a-um com a natureza primordial. E, assim o fez, transformando a cidade de Haprinas em caos subitamente, trazendo terror para todos.";
+        usleep(5000000);
+
+        limpar();//limpar console
+
+        //Dialogo 4
+        cout<<"\n\nNo mesmo dia, horas depois... Ao por do sol em um beco da cidade de Haprinas, um homem de cabelos escuros e olhos castanhos aparece saindo de uma loja de pocoes... Protagonista: Mais um dia sem conseguir emprego, que tragedia! Este sou eu, Klein! (Resmungando internamente) Que piada... Ah droga, preciso ir logo para casa, esta anoitecendo.";
+        usleep(5000000);
+
+        limpar();//limpar console
+
+        //Dialogo 5
+        cout<<"\n***BOOOOOM!!!! *ESTRONDO* - Sons de pessoas gritando em panico ao fundo e logo apos um silencio assustador. Klein: O que? Mas o que foi isso? O que sera que devo fazer? Esse barulho parece ter vindo do centro da cidade, mas... Sera que devo ir averiguar?";
+        usleep(5000000);
+
+        //Dialogo 6
+        cout<<"\n\n{DIGITE 1 PARA IR AVERIGUAR} | {DIGITE 2 PARA FUGIR E CORRER}\n\n";
+        cin>>escolha1;
+
+        limpar();//limpar console
+        
+        if(escolha1 == 1){
+
+            cout<<"\nKlein vai averiguar o caos que estava acontecendo na cidade antes de ser substituido pelo silencio assustador, ao longo do seu trajeto ate a regiao central da cidade ele conseguiu observar centenas de ramos de arvores sobrenaturais enfincados em incontaveis pessoas, matando e sugando o sangue das pessoas, absorvendo sua energia vital. Klein: Que coisa bizarra esses ramos de arvores extensos, parecem estar conectados a um unico local, como se todas derivassem da mesma fonte, isso e estranho. Ainda bem que meu antigo trabalho como coveiro me fortificou para aguentar essa cena perturbadora.";
+                
+                limpar();//limpar console
+
+                //Dialogo 7
+                usleep(5000000);
+                cout<<"\nKlein ao longo de sua caminhada ate a fonte dos ramos, ele observa uma loja de ferreiro, onde pode ser possivel obter algum equipamento util, deseja entrar?";
+                cout<<"\n\n{DIGITE 1 PARA ENTRAR NA LOJA} | {DIGITE 2 PARA IGNORAR}\n\n";
+                cin>>escolha2;
+        
+                limpar();//limpar console
+                
+                if(escolha2 == 1) {
+                    cout<<"\nKlein tenta entrar na loja do ferreiro, no entanto, a porta estava trancada, ao vasculhar aos arredores, ele encontra um anao morto, que ele reconhece como sendo o dono do estabelecimento, o anao havia acabado de fechar a loja quando um ramo de arvore sobrenatural perfurou suas costas e destruiu seus pulmoes, assim absorvendo a energia vital dele. Klein: Descanse em paz, amigo, espero que a vida seja mais legal com voce na proxima chance. *Vasculha corpo do ferreiro e encontra a chave para desbloquear a porta. \n\n(VOCE OBTEVE UMA ARMADURA ESFARRAPADA)";
+                    
+                    cout<<"\n\nKlein durante sua caminhada, ele com sua criatividade imagina que o que esteja sendo responsavel por fazer todo esse caos, seja algum monstro igual nas historias de terror que ele escutava de seus pais quando ainda estavam vivos, e por se tratar de um possivel monstro com poder de ramos de arvore, ele concluiu que a forma mais eficiente de o derrotar seria utilizando fogo, mas para isso ele precisaria de algo para intensificar as chamas.";
+                    usleep(5000000);
+                    
+                    limpar();//limpar console
+                    
+                    cout<<"\nKlein: Eu preciso encontrar uma forma de conseguir criar uma especie de fogueira gigante para queimar toda essa coisa, mas, onde sera que devo achar algo assim?";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    cout<<"\nKlein estava caminhando e olhando para os lados, quando visualiza o celeiro da cidade, e surge uma ideia genial.";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    //Dialogo 9
+                    cout<<"\nDentro do celeiro estava infestado dos ramos de arvores sobrenaturais tambem, com isso, Klein que nao havia tocado nesses ramos, decidiu provocar esses ramos, batendo com um pedaco de pedra que encontrou dentro do celeiro, com isso, ele comecou a ouvir tremores de algo se aproximando e se esgueirou para algo que servisse de cobertura para ele.";
+                    usleep(5000000);
+                    
+                    limpar();//limpar console
+                    
+                    cout<<"\nKlein: Vamos la, vamos la!!";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    //Dialogo 10
+                    cout<<"\nRodrigo ao sentir que seus ramos estavam sendo atacados, ele decidiu averiguar o que estava acontecendo, pois ele ainda nao tinha controle suficiente do seu poder para verificar a longa distancia. Apos algum tempo ele chegou em um celeiro antigo e velho da cidade e estava tentando identificar o que causou danos a ele.";
+                    usleep(5000000);
+                    
+                    limpar();//limpar console
+                    
+                    cout<<"\nKlein: Eh agora! Toma essa monstro!";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    cout<<"\n(KLEIN DO LADO DE FORA DO CELEIRO ARREMESSOU AS VARIAS TOCHAS QUE REUNIU DO LUGAR E INCENDIOU O LOCAL EM UM PISCAR DE OLHOS, POR CONTA DA QUANTIDADE DE FENO CONCENTRADO EM TODO O AMBIENTE)";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    //Dialogo 11 Final 2
+                    cout<<"\nRodrigo: NAAOOO!! Eu vou matar voce! -Rodrigo tenta atingir com seus ramos de arvores sobrenaturais o protagonista, no entanto, a armadura esfarrapada impede o contato do ramo da arvore com sua propria pele, o que faz com que o salve nos poucos segundos de combate com o monstro.";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    cout<<"\nPor se tratar de uma magia profana que nao eh aceita nesse mundo por leis naturais, atraves do momento de fraqueza do Rodrigo lutando para sobreviver contra o fogo queimando seu corpo, a propria natureza cuidou dele, extinguindo-o para sempre do mundo.";
+                    usleep(5000000);
+                    
+                    limpar();//limpar console
+                    
+                    cout<<"\nKlein: Que loucura tudo isso... Parece que foi tudo um sonho, a maior parte dos cidadaos devem ter sido atingidos por esse louco..., mas olhando para um lado bom, eu nao vou precisar arranjar emprego tao cedo, so preciso saquear as lojas para me manter.";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    cout<<"\n\nVoce finalizou a historia seguindo o final onde o protagonista derrota o vilao e permanece vivo.";
+                    cout<<"\nMensagem: Final 2, recomece para descobrir os outros finais";
+                        
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    return 0;
+                    }
+                    
+                if(escolha2 == 2) {
+                    cout<<"\n\nKlein ignorou a loja e seguiu adiante.";
+                    
+                    cout<<"\nKlein durante sua caminhada, ele com sua criatividade imagina que o que esteja sendo responsavel por fazer todo esse caos, seja algum monstro igual nas historias de terror que ele escutava de seus pais quando ainda estavam vivos, e por se tratar de um possivel monstro com poder de ramos de arvore, ele concluiu que a forma mais eficiente de o derrotar seria utilizando fogo, mas para isso ele precisaria de algo para intensificar as chamas.";
+
+                    limpar();//limpar console
+
+                    cout<<"\nKlein: Eu preciso encontrar uma forma de conseguir criar uma especie de fogueira gigante para queimar toda essa coisa, mas, onde sera que devo achar algo assim?";
+
+                    limpar();//limpar console
+
+                    cout<<"\nKlein estava caminhando e olhando para os lados, quando visualiza o celeiro da cidade, onde surge uma ideia genial.";
+                    usleep(5000000);
+                    
+                    limpar();//limpar console
+
+                    //Dialogo 2 Final 3
+                    cout<<"\nDentro do celeiro, estava infestado dos ramos de arvores sobrenaturais tambem, com isso, Klein que nao havia tocado nesses ramos, decidiu provocar esses ramos, batendo com um pedaco de pedra que encontrou dentro do celeiro, com isso, ele comecou a ouvir tremores de algo se aproximando e se esgueirou para algo que servisse de cobertura para ele.";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    cout<<"\nKlein: Vamos la, vamos la!!";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    //Dialogo 3 Final 3
+                    cout<<"\nRodrigo ao sentir que seus ramos estavam sendo atacados, ele decidiu averiguar o que estava acontecendo, pois ele ainda nao tinha controle suficiente do seu poder para verificar a longa distancia. Apos algum tempo ele chegou em um celeiro antigo e velho da cidade e estava tentando identificar o que causou danos a ele.";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    cout<<"\nKlein: Eh agora! Toma essa monstro!";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    cout<<"\n(KLEIN DO LADO DE FORA DO CELEIRO ARREMESSOU AS VARIAS TOCHAS QUE REUNIU DO LUGAR E INCENDIOU O LOCAL EM UM PISCAR DE OLHOS, POR CONTA DA QUANTIDADE DE FENO CONCENTRADO EM TODO O AMBIENTE)";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    //Dialogo 4 Final 3
+                    cout<<"\nRodrigo: NAAOOO!! Eu vou matar voce!";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+                    cout<<"\n-Rodrigo atinge o protagonista e desfere uma ferida mortal em Klein.";
+                    
+                    limpar();//limpar console
+                    cout<<"\nPor se tratar de uma magia profana que nao eh aceita nesse mundo por leis naturais, atraves do momento de fraqueza do Rodrigo lutando para sobreviver contra o fogo queimando seu corpo, a propria natureza cuidou dele, extinguindo-o para sempre do mundo, restando apenas os ramos de arvores mortos espalhados por toda a cidade. E Klein, tendo se sacrificado para salvar a todos, sera esquecido na memoria das pessoas, mas para nos, jogadores, sera lembrado para sempre.";
+                    usleep(5000000);
+            
+                    limpar();//limpar console
+            
+                    cout<<"\n\nVoce finalizou a historia seguindo o final onde o protagonista derrota o vilao e eh morto no processo.";
+                    cout<<"\nMensagem: Final 3, recomece para descobrir os outros finais!";
+            
+                    return 0;
+                }
+            }   else if(escolha1 == 2){
+                    cout<<"Voce finalizou a historia seguindo o final apelidado de COVARDE !!   \n\nMensagem: Final 1, recomece para descobrir os outros finais!\n\n"; //Dialogo Final 1
+                     return 0;
+                }   else {
+                        cout<<"Voce digitou um numero invalido, tente novamente do inicio";
+                        
+                        return 0;
+                    }
+        }
